8c05ca9334a5e83a9baebe1ee3d16a749273feab
WantunXD <rechngml@gmail.com>
2024-02-10 13:27:56 +0900

    簡易認証ページ

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <header className="bg-green-200 h-20">
+        header
+      </header>
+    </div>
+  )
+}
+
+export default Header
