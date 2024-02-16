71bb84bcb10793f3c2e1b7721b7bc15b95ef5330
WantanXD <rechngml@gmail.com>
2024-02-16 16:09:08 +0900

    問題出題画面の作成途中（メニュー部分）

--- src/pages/InteractiveQandA/index.tsx
+++ src/pages/InteractiveQandA/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react'
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import StartButton from "./StartButton";
+
+// 最初に選択画面 & ボタンを配置
+// 初期画面で、出題言語を選択し、問題出題&解答ボタンを押して開始
 
 function interactiveQandA() {
 
+  const [monitorState,setMonitorState] = useState("MENU");
+
   return (
-    <div className='interactiveQandA'>
-      
-    </div>
+    <main className='interactiveQandA h-screen'>
+      <div className="Header">
+        <Header/>
+      </div>
+      <div className="MainContainer h-full flex">
+        <Sidebar/>
+        <div className='MyBody'>
+          {
+          monitorState === "MENU" ? // 言語の選択とかをするページ
+          <StartButton/>
+          :
+          
+          ;
+          }
+        </div>
+      </div>
+    </main>
   )
 }
 
