2f2cddebbff7b02ea7f4a4e07d7abdfc1c142caa
WantanXD <rechngml@gmail.com>
2024-02-16 18:38:22 +0900

    画面遷移用ボタンを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
-import Link from "next/link";
+import NextLink from "next/link";
+import {Button, Link as MuiLink} from "@mui/material/";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// 最初に選択画面 & ボタンを配置
+// 初期画面で、出題言語を選択し、問題出題&解答ボタンを押して開始
+
 export default function Home() {
+
   return (
     <main className="h-screen">
       <div className="Header">
@@ -15,11 +20,10 @@ export default function Home() {
       <div className="MainContainer h-full flex">
         <Sidebar/>
         <div className="MyBody">
-          <Link className="Authenticate" href="./Authenticate">あああ</Link>
+          <NextLink href="./InteractiveQandA" passHref>
+            <Button className="StartButton" variant='outlined'>問題を解く</Button>
+          </NextLink>
         </div>
-      </div>
-      <div>
-        <Link href="./InteractiveQandA">しゅつだい</Link>
       </div>
     </main>
   );
