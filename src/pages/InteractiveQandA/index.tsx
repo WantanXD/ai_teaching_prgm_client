a0e63056a7cf5034a76c302718533521a8016d44
WantanXD <rechngml@gmail.com>
2024-02-19 10:23:27 +0900

    メイン画面CSSの軽微な修正

--- src/pages/InteractiveQandA/index.tsx
+++ src/pages/InteractiveQandA/index.tsx
@@ -11,10 +11,18 @@ const InteractiveQandA = () => {
   const [tof, setTof] = useState<string | null>(null);
   const [comments, setComments] = useState<Array<string>>([]);
   const answerRef = useRef<HTMLTextAreaElement | null>(null);
-  let submitLock = false;
+  const [isSendAnswer, setIsSendAnswer] = useState<boolean>(false);
+
+  const initStatus = () => {
+    setIsSendAnswer(false);
+    setQuestion(null);
+    setCount(0);
+    setTof(null);
+    setComments([]);
+  }
 
   const generateQuestion = async() => {
-    const programmingLang = "C";
+    const programmingLang = "Java";
     await apiClient.post("/gemini/generateQuestion", {
       pl:programmingLang
     }).then((responce: any) => {
@@ -30,8 +38,8 @@ const InteractiveQandA = () => {
 
   const hundleSubmit = async(e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (submitLock === false) {
-      submitLock = true;  // 2回目のsubmitを無効化する
+    if (isSendAnswer === false) {
+      setIsSendAnswer(true);  // 2回目のsubmitを無効化する
       const answer = answerRef.current !== null ? answerRef.current?.value : "";
       if (answer !== "") {
         await apiClient.post("gemini/answerCheck", {
@@ -47,6 +55,7 @@ const InteractiveQandA = () => {
   };
 
   useEffect(() => {
+    initStatus();
     generateQuestion();
   }, []);
 
@@ -62,37 +71,34 @@ const InteractiveQandA = () => {
       <div className="MainContainer h-full flex">
         <Sidebar/>
         <div className="MyBody">
+          <div className="QandABody">
           {question !== null && (
-            <div>
-              {question.split('\n').map((line, index) => (
-                <React.Fragment key={index}>
-                  <ReactMarkdown>{line}</ReactMarkdown>
-                </React.Fragment>
-              ))}
+            <div className="Question">
+              <ReactMarkdown>{question.replace('\n', '')}</ReactMarkdown>
             </div>
           )}
           {question !== null && (
-            <div>
+            <div className="Answer">
               <h2>解答を入力</h2>
               <form onSubmit={hundleSubmit}>
-                <textarea rows={4} cols={40} name="answer" placeholder="解答を入力" ref={answerRef}/>
-                <input type="submit" value="解答"/>
+                <textarea className="Answer-text" rows={8} name="answer" ref={answerRef}/>
+                {isSendAnswer === false && (<input type="submit" value="解答"/>)}
               </form>
             </div>
           )}
+        </div>
+        <div className="DescBody">
           {tof !== null && comments[0] !== "" && (
-            <div>
-              <h1>--------------------------------------</h1>
+            <div className="Description">
               <p>{tof === 'Y' ? "正解" : "不正解"}</p>
               {comments.map((comment, index) => (
-                comment.split('\n').map((line, i) => (
-                  <React.Fragment key={i}>
-                    <ReactMarkdown>{line}</ReactMarkdown>
-                  </React.Fragment>
-                ))
+                <ReactMarkdown>
+                  {comment !== 'Y' && comment !== 'Great' ? comment.replace('\n', '') : ''}
+                </ReactMarkdown>
               ))}
             </div>
           )}
+          </div>
         </div>
       </div>
     </div>
