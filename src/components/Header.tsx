4bc7643b1f9bed75420f4215633d09b27d06bea1
WantanXD <rechngml@gmail.com>
2024-03-25 16:14:50 +0900

    ログインボタンの追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import NextLink from "next/link";
+import { Button } from '@mui/material';
 
 function Header() {
   return (
     <div>
       <header className="Header bg-green-200">
-        header
+        <div className='loginButton'>
+          <NextLink href="/Authenticate" passHref>
+            <Button className='loginButton' variant='outlined' color='inherit'>ログイン</Button>
+          </NextLink>
+        </div>
       </header>
     </div>
   )
