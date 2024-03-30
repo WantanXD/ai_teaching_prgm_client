a249ea47974b062655fdf9671aa93e52e9db3d08
WantanXD <rechngml@gmail.com>
2024-03-30 16:16:17 +0900

    apiサーバを環境変数に変更

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL:"http://localhost:4649/api",
+    baseURL:process.env.NEXT_PUBLIC_API_IP,
     headers:{
         "Content-Type":"application/json"
     }
